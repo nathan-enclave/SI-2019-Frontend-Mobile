ed40f3e53927ce39b3f8d7e61fb372b6f1cbf1cd
TUUED <tunguyen.ued@gmail.com>
2019-07-16 09:12:21 +0700

    clean code

--- app/src/main/java/com/example/enclavemobileapp/LoginActivity.java
+++ app/src/main/java/com/example/enclavemobileapp/LoginActivity.java
@@ -9,7 +9,6 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
