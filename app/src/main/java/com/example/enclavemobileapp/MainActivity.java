c8a08afc517d9e203a729e9d13628532c2c16e1d
TUUED <tunguyen.ued@gmail.com>
2019-08-07 20:58:43 +0700

    project list

--- app/src/main/java/com/example/enclavemobileapp/MainActivity.java
+++ app/src/main/java/com/example/enclavemobileapp/MainActivity.java
@@ -164,6 +164,8 @@ public class MainActivity extends AppCompatActivity
         llProjects.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProjectActivity.class);
+                startActivity(intent);
             }
         });
 
